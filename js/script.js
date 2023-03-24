47a3bb70b5b21037b399361557965fee0efa3c08
janet-do <124389477+janet-do@users.noreply.github.com>
2023-03-24 16:46:25 +0000

    dropped template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Janet Do All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Janet Do
 // Created on: Sep 2020
-// This file contains the JS functions for index.html
+// This file generates the area of a triangle
+"use strict"
+function calculate() {
+  // input
+  const base = parseInt(document.getElementById("base").value)
+  const height = parseInt(document.getElementById("height").value)
+
+  // process
+  const area = base * height / 2
+
+  // output
+  document.getElementById('pay').innerHTML = `Your pay will be: $ ${takeHomeSalary.toFixed(2)}`
+  document.getElementById('pay').innerHTML = 'Your pay will be: $' + takeHomeSalary.toFixed(2)
+}
