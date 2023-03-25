2cd9bfe110361a56041d564bb36ac2ec597e3945
janet-do <124389477+janet-do@users.noreply.github.com>
2023-03-25 22:57:12 +0000

    added title

--- js/script.js
+++ js/script.js
@@ -8,21 +8,15 @@ function calculate() {
   // input
   const hours = parseInt(document.getElementById("hours").value)
   const rate = parseInt(document.getElementById("rate").value)
-  xx = parseFloat(document.getElementById("pay").value)
 
   // process
   const pay = hours * rate * (1 - 0.18)
   const tax = hours * rate * 0.18
-
+  
   // output
-  document.getElementById(
-    "pay"
-  ).innerHTML = `Your pay will be: $ ${pay.toFixed(2)}`
-  document.getElementById("pay").innerHTML =
-    "Your pay will be: $" + pay.toFixed(2)
-    document.getElementById(
-      "tax"
-    ).innerHTML = `Your pay will be: $ ${tax.toFixed(2)}`
-    document.getElementById("pay").innerHTML =
-      "The government will take: $" + tax.toFixed(2)
+  document.getElementById("pay").innerHTML = `Your pay will be: $ ${pay.toFixed(2)}`
+  document.getElementById("pay").innerHTML = "Your pay will be: $" + pay.toFixed(2)
+
+  document.getElementById("tax").innerHTML = `Your pay will be: $ ${tax.toFixed(2)}`
+  document.getElementById("tax").innerHTML = "The government will take: $" + tax.toFixed(2)
 }
