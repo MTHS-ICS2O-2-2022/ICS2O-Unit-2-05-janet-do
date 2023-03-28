dca0da9633a3ecd7f583b8f16af26f0e96adbcad
janet-do <124389477+janet-do@users.noreply.github.com>
2023-03-28 22:12:03 +0000

    fixed

--- go_app/main.go
+++ go_app/main.go
@@ -12,18 +12,21 @@ import (
 const taxRate = 0.18
 
 func main() {
-	//input
+	// input
 	var hours, rate int
 	fmt.Print("Enter the number of hours worked: ")
 	fmt.Scanln(&hours)
 	fmt.Print("Enter the hourly rate: ")
 	fmt.Scanln(&rate)
+	fmt.Println() // add an empty line here
 
-	//proccess
+	// proccess
 	pay := float64(hours*rate) * (1 - taxRate)
 	tax := float64(hours*rate) * taxRate
-	
-	//output
+
+	// output
 	fmt.Printf("Your pay will be: $%.2f\n", pay)
 	fmt.Printf("The government will take: $%.2f\n", tax)
+
+	fmt.Println("\nDone.")
 }
--- js/script.js
+++ js/script.js
@@ -2,21 +2,20 @@
 //
 // Created by: Janet Do
 // Created on: Sep 2020
-// This file generates the area of a triangle
+// This file generates pay and tax deductions
+
 "use strict"
 function calculate() {
+  const deductions = 0.18
   // input
   const hours = parseInt(document.getElementById("hours").value)
   const rate = parseInt(document.getElementById("rate").value)
 
   // process
-  const pay = hours * rate * (1 - 0.18)
-  const tax = hours * rate * 0.18
+  const pay = hours * rate * (1 - deductions)
+  const tax = hours * rate * deductions
   
   // output
-  document.getElementById("pay").innerHTML = `Your pay will be: $ ${pay.toFixed(2)}`
   document.getElementById("pay").innerHTML = "Your pay will be: $" + pay.toFixed(2)
-
-  document.getElementById("tax").innerHTML = `Your pay will be: $ ${tax.toFixed(2)}`
   document.getElementById("tax").innerHTML = "The government will take: $" + tax.toFixed(2)
 }
